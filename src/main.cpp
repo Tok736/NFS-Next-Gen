d510990ce88e69793d1e7a9a1ab9cdf7ea7f933d
margot07008 <9311margarita@gmail.com>
2020-04-20 11:57:40 +0300

    uodate cmake

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,10 @@
 #include "sum/sum.h"
 #include <iostream>
+#include <SFML/Graphics.hpp>
+
 
 int main() {
+
     std::cout << "Hello!" << std::endl;
     std::cout << "5 + 7 = " << sum(5, 7) << std::endl;
 
