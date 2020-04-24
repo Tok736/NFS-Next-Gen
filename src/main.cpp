4e225870a7329a041443de36ecc58fdbd9678f00
H-b-IO-T-O-H <importantv@yandex.ru>
2020-04-24 13:08:26 +0300

    asr

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,23 @@
 #include "sum/sum.h"
 #include <iostream>
+#include <sqlite3.h>
 
 int main() {
-    std::cout << "Hello!" << std::endl;
-    std::cout << "5 + 7 = " << sum(5, 7) << std::endl;
-
-    return 0;
+	sqlite3 *db;
+	
+	int rc;
+	
+	rc = sqlite3_open("test.db", &db);
+	
+	if (rc) {
+		fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+		return (0);
+	} else {
+		fprintf(stderr, "Opened database successfully\n");
+	}
+	sqlite3_close(db);
+	std::cout << "Hello!" << std::endl;
+	std::cout << "5 + 7 = " << sum(5, 7) << std::endl;
+	
+	return 0;
 }
