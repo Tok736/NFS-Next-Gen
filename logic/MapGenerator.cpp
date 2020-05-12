3b9429faf2b1f5efd3a2b5be6b4dd8e533badc0d
Max <max21011999@mail.ru>
2020-05-12 19:21:05 +0300

    nothing

--- logic/MapGenerator.cpp
+++ logic/MapGenerator.cpp
@@ -1,12 +1,12 @@
 #include "MapGenerator.h"
 #include <iostream>
 
-#define width 1440
+#define width 672  //ширина дороги [378;1050]
 #define height 900
 
 MapGenerator::MapGenerator(short difficulty) : difficulty(difficulty) {
     this->generate();
-    
+
 //    for (int i = 0; i < mapVector.size(); ++i) {
 //        std::cout << "x: " << mapVector[i]->getX() << " y: " << mapVector[i]->getY() << std::endl;
 //    }
@@ -20,7 +20,7 @@ void MapGenerator::generate() {
     int columnWidth = width / columnAmount;
     std::srand(time(NULL));
     for (int i = columnWidth / 2; i <= koef * difficulty; i += columnWidth) {
-        mapVector.push_back(Obstruction(0, columnWidth / 2 + columnWidth * (rand() % columnAmount), i));
+        mapVector.push_back(Obstruction(0, 378 + columnWidth / 2 + columnWidth * (rand() % columnAmount), i));
     }
 }
 
